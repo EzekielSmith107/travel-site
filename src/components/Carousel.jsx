0f09fa229c31544eb18bb4ab946a6459eb47832d
EzekielSmith107 <ezesmith107@gmail.com>
2022-09-23 14:42:28 -0400

    carousel functional

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import chef from '../assets/chef.jpg'
 import fuji from '../assets/fuji.jpg'
 import sakura from '../assets/sakura.jpg'
+import { BsArrowLeftSquareFill, BsArrowRightSquareFill } from 'react-icons/bs'
 
 const Carousel = () => {
     const sliderData = [
@@ -11,9 +12,19 @@ const Carousel = () => {
     ]
 
     const [ slide, setSlide ] = useState(0)
+    const length = sliderData.length
+
+    const prevSlide = () => {
+        setSlide(slide === 0 ? length - 1 : slide - 1)
+    }
+    const nextSlide = () => {
+        setSlide(slide === length - 1 ? 0 : slide + 1)
+    }
   
     return (
     <div className='max-w-[1240px] mx-auto px-4 py-16 relative flex justify-center items-center'>
+        <BsArrowLeftSquareFill onClick={prevSlide} className='absolute top-[50%] text-3xl text-white cursor-pointer left-8' />
+        <BsArrowRightSquareFill onClick={nextSlide} className='absolute top-[50%] text-3xl text-white cursor-pointer right-8' />
         { sliderData.map((item, index) => (
             <div className={ index === slide ? 'opacity-100' : 'opacity-0' }>
                 { index === slide && (<img className='w-full rounded-md' src={item.image} alt={item.alt} />) }
